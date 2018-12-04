50217e97d27349923492fd448192045a6f7d4370
tetsushi kawarabata <kwrbt0404@gmail.com>
2018-12-04 20:33:28 +0900

    Delete Pokemon class

---
+++ pokemon_ruby2/battle_start.rb
@@ -0,0 +1,14 @@
+require './pokemon_list'
+require './PokemonBattle_class'
+
+# battle1 = PokemonBattle.new
+# battle1.encount
+# battle1.select_mon
+# print "push Enter..."
+# wait = gets
+# puts "\n------------------------\n\n"
+# battle1.battle_menu
+
+encount
+select_monster
+battle_menu
