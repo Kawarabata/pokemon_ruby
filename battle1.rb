939ffa56fe7047fc070689b761b9686de0a7d924
Kawarabata <tetsushi.kawarabata@cluex.co.jp>
2018-12-04 13:27:34 +0900

    Finish battle1

---
+++ battle1.rb
@@ -0,0 +1,7 @@
+require "./classes"
+
+battle_pokemon1 = Pikachu.new("ピカ太郎")
+battle_pokemon1.jump_out
+battle_pokemon1.attack1
+battle_pokemon1.attack2
+battle_pokemon1.escape
--- classes.rb
+++ classes.rb
@@ -1,6 +1,20 @@
 class Pokemon
   def initialize(name)
     @name = name
-    puts "ゆけっ #{name}!"
+    puts "ゆけっ #{@name}!"
+  end
+  def jump_out
+    puts "#{@name}、かわせ!"
+  end
+  def escape
+    puts "もどれ、#{@name}"
+  end
+end
+class Pikachu < Pokemon
+  def attack1
+    puts "#{@name}、10万ボルト!"
+  end
+  def attack2
+    puts "#{@name}、アイアンテールだ！"
   end
 end
