14e2ffce1ae45fea9afbcb86320ad2aea040f3b0
tetsushi kawarabata <kwrbt0404@gmail.com>
2018-12-05 23:45:29 +0900

    Fix HP bar

--- battle_system.rb
+++ battle_system.rb
@@ -2,12 +2,16 @@ require './pokemon_class'
 
 @my_partner = MyPokemon.new
 @enemy = EnemyPokemon.new
-
+@total_damage1 = 0
+@total_damage2 = 0
 def attack
   damage = @enemy.decrease_hp
+  @total_damage1 += damage
   message "#{$name}の こうげき!"
   message "#{$enemy}に#{damage}ダメージ"
+  print ("HP ")
   @enemy.hp.times {print ('|')}
+  @total_damage1.times {print ('.')}
   space
   if @enemy.hp <= 0
     message "#{$enemy}は たおれた!" 
@@ -18,9 +22,12 @@ end
 
 def enemy_turn
   damage = @my_partner.decrease_hp
+  @total_damage2 += damage
   message "#{$enemy}の こうげき!"
   message "#{$name}に#{damage}ダメージ"
+  print ("HP ")
   @my_partner.hp.times {print ('|')} 
+  @total_damage2.times {print ('.')}
   space
   if @my_partner.hp <= 0
     message "#{$name}は たおれた!" 
